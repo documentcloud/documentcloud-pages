9e01c15df005762e9d028ffb980b0489290f920f
Ted Han <ted@knowtheory.net>
2014-11-03 13:37:22 -0600

    Setup structure to hold views and wire up view creation.

--- js/page_embed.js
+++ js/page_embed.js
@@ -35,13 +35,23 @@
   });
   
   data.documents = data.documents || new definition.DocumentSet();
-  views.pages = views.pages || new Backbone.Collection();
+  // views.pages is a nested list of page views, keyed at the top level
+  // by document id, and then element selector.
+  // e.g. views.pages['282753-lefler-thesis']['#target-container']
+  // initialization of the inner object is done in dc.embed.loadPage
+  views.pages = views.pages || {};
   
   dc.embed.loadPage = function(url, opts){
     var options = opts || {};
+    // should throw an error here if there isn't a container.
     var id = definition.Document.extractId(url);
+    
+    // create the things and keep references to them in the right places.
     var doc = new definition.Document({id: id});
+    var view = new definition.PageView({model: doc, el: options.container, page: options.page});
     data.documents.add(doc);
-    doc.fetch({url: url});
+    views.pages[id] = views.pages[id] || {};
+    views.pages[id][options.container] = view;
+    doc.fetch({url: url}); // kick everything off.
   };
 })();
