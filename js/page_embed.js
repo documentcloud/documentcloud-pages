de2ca8892eaa0784d1dc7d61a3834c3a90fcca05
Ted Han <ted@knowtheory.net>
2014-11-03 12:07:40 -0600

    Cleanup dependencies, add model definition & entry point for loading.

--- js/page_embed.js
+++ js/page_embed.js
@@ -1,15 +1,26 @@
 (function(){
-  window.dc = dc = window.dc || {};
-  dc.$ = window.$.noConflict();
-  dc._ = window._.noConflict();
-  dc.Backbone = window.Backbone.noConflict();
+  window.dc    = dc = window.dc || {};
+  var $        = dc.$ = window.$.noConflict();
+  var _        = dc._ = window._.noConflict();
+  var Backbone = dc.Backbone = window.Backbone.noConflict();
 
   dc.embed = dc.embed || { data: {}, definition: {} };
   
   var definition = dc.embed.definition;
   var data = dc.embed.data;
   
-  dc.embed.loadPage = function(){
-    
+  definition.Document = Backbone.Model.extend({}, {
+    extractId: function(url){ return url.match(/(\d+[A-Za-z-]+).js(on)?$/)[1]; }
+  });
+  definition.DocumentSet = Backbone.Collection.extend({ model: definition.Document });
+  
+  data.documents = new definition.DocumentSet();
+
+  dc.embed.loadPage = function(url, opts){
+    var options = opts || {};
+    var id = definition.Document.extractId(url);
+    var doc = new definition.Document({id: id});
+    data.documents.add(doc);
+    doc.fetch({url: url});
   };
 })();
