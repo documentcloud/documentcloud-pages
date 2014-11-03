57f878a1e63a0fadc304280977445e935597b309
Ted Han <ted@knowtheory.net>
2014-11-03 12:45:13 -0600

    Setup views.

--- js/page_embed.js
+++ js/page_embed.js
@@ -4,18 +4,26 @@
   var _        = dc._ = window._.noConflict();
   var Backbone = dc.Backbone = window.Backbone.noConflict();
 
-  dc.embed = dc.embed || { data: {}, definition: {} };
+  dc.embed = dc.embed || { data: {}, definition: {}, views: {} };
   
   var definition = dc.embed.definition;
   var data = dc.embed.data;
-  
-  definition.Document = Backbone.Model.extend({}, {
+  var views = dc.embed.views;
+
+  definition.Document = definition.Document || Backbone.Model.extend({}, {
     extractId: function(url){ return url.match(/(\d+[A-Za-z-]+).js(on)?$/)[1]; }
   });
-  definition.DocumentSet = Backbone.Collection.extend({ model: definition.Document });
-  
-  data.documents = new definition.DocumentSet();
+  definition.DocumentSet = definition.DocumentSet || Backbone.Collection.extend({ 
+    model: definition.Document 
+  });
 
+  definition.PageView = definition.PageView || Backbone.View.extend({
+    template: JST['page']
+  });
+  
+  data.documents = data.documents || new definition.DocumentSet();
+  views.pages = views.pages || new Backbone.Collection();
+  
   dc.embed.loadPage = function(url, opts){
     var options = opts || {};
     var id = definition.Document.extractId(url);
