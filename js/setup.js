e78e00bd2fd15a55b3bf5570412421c79ffb6edd
Justin Reese <justin@justinreese.com>
2015-09-21 11:34:16 -0500

    Reference jQuery as `jQuery` during setup

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 (function(){
   var DocumentCloud = window.DocumentCloud = window.DocumentCloud || {
-    $: window.$.noConflict(),
-    _: window._.noConflict(),
+    $:        window.jQuery.noConflict(),
+    _:        window._.noConflict(),
     Backbone: window.Backbone.noConflict()
   };
   DocumentCloud.embed = DocumentCloud.embed || { data: {}, definition: {}, views: {} };
