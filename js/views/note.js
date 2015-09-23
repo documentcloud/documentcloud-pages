d3714c2bc8b7c892817d130c0cc1a63fa19b09b6
Justin Reese <justin@justinreese.com>
2015-09-23 17:17:04 -0500

    Revert 3f413f4b9225229f87a301ddb05d475d858bd9f3

--- js/views/note.js
+++ js/views/note.js
@@ -54,14 +54,16 @@
     },
   
     open: function() {
-      this.$el.addClass('open').closest('.DC-embed').addClass('open');
+      this.$el.addClass('open');
       this.trigger('opened', this);
+      this.$el.closest('.DC-embed').addClass('open');
       this.repositionIfNecessary();
     },
   
     close: function() {
-      this.$el.removeClass('open').closest('.DC-embed').removeClass('open');
+      this.$el.removeClass('open');
       this.trigger('closed', this);
+      this.$el.closest('.DC-embed').removeClass('open');
     },
 
     // Note might drip off right edge; reposition in that case.
