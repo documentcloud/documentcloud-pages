3f413f4b9225229f87a301ddb05d475d858bd9f3
Justin Reese <justin@justinreese.com>
2015-09-23 17:03:29 -0500

    Chain note-open/closing class name changes

--- js/views/note.js
+++ js/views/note.js
@@ -54,16 +54,14 @@
     },
   
     open: function() {
-      this.$el.addClass('open');
+      this.$el.addClass('open').closest('.DC-embed').addClass('open');
       this.trigger('opened', this);
-      this.$el.closest('.DC-embed').addClass('open');
       this.repositionIfNecessary();
     },
   
     close: function() {
-      this.$el.removeClass('open');
+      this.$el.removeClass('open').closest('.DC-embed').removeClass('open');
       this.trigger('closed', this);
-      this.$el.closest('.DC-embed').removeClass('open');
     },
 
     // Note might drip off right edge; reposition in that case.
