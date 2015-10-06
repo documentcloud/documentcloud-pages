d39d8e4940ff8c8dbbdafceba8e96620f381105f
Justin Reese <justin@justinreese.com>
2015-10-06 14:16:26 -0500

    Organize and comment loader.js
    * Functionalize logical blocks
    * Add useful comments
    * Camelcase and make more explicit some variable names

--- js/loader.js
+++ js/loader.js
@@ -2,15 +2,41 @@
 
   Penny.ready(function(){
 
-    var generateUniqueElementId = function(doc, page) {
+    // Insert the necessary stylesheet into the head, unless it's already there.
+    var insertStylesheet = function() {
+      if (!document.querySelector('link[href$="page_embed.css"]')) {
+        var stylesheet   = document.createElement('link');
+        stylesheet.rel   = 'stylesheet';
+        stylesheet.type  = 'text/css';
+        stylesheet.media = 'screen';
+        stylesheet.href  = 'dist/page_embed.css';
+        document.querySelector('head').appendChild(stylesheet);
+      }
+    };
+
+    // Insert the necessary JavaScript into the body, unless it's already there.
+    var insertJavaScript = function() {
+      if (!document.querySelector('script[src$="page_embed.js"]')) {
+        var page_embed_js = document.createElement('script');
+        page_embed_js.src = "dist/page_embed.js";
+        Penny.on(page_embed_js, 'load', enhanceStubs);
+        document.querySelector('body').appendChild(page_embed_js);
+      }
+    };
+
+    // Generates a unique ID for a given page, checks the DOM for any existing
+    // element with that ID, then increments and tries again if it finds one.
+    var generateUniquePageElementId = function(documentSlug, pageNumber) {
       var i = 1;
-      var id = doc + '-p' + page + '-i' + i;
+      var id = documentSlug + '-p' + pageNumber + '-i' + i;
       while (document.getElementById(id)) {
         id = id.replace(/-i[0-9]+$/, '-i' + i++);
       }
       return id;
     };
 
+    // Takes an embed stub (the entire DOM element) and looks for a 
+    // `data-options` attribute that contains a JSON representation of options.
     var extractOptionsFromStub = function(stub) {
       var options = stub.getAttribute('data-options');
       if (options) {
@@ -27,27 +53,31 @@
       return options;
     };
 
+    // Convert embed stubs to real live embeds. If this fails, stubs remain 
+    // usable as effectively `noscript` representations of the embed.
     var enhanceStubs = function() {
       var stubs = document.querySelectorAll('.DC-embed-stub');
       Penny.forEach(stubs, function (stub, i) {
-        var href        = stub.querySelector('.DC-embed-resource').getAttribute('href');
+        var href = stub.querySelector('.DC-embed-resource').getAttribute('href');
         // TODO: Recognize resource type based on URL pattern and load
         //       appropriate embed mechanism.
-        var components  = href.match(/\/documents\/([A-Za-z0-9-]+)\.html\#document\/p([0-9]+)$/);
+        var components = href.match(/\/documents\/([A-Za-z0-9-]+)\.html\#document\/p([0-9]+)$/);
         if (components) {
-          var document_id = components[1];
-          var page_number = components[2];
-          var element_id  = generateUniqueElementId(document_id, page_number);
+          var documentSlug = components[1];
+          var pageNumber   = components[2];
+          var elementId    = generateUniquePageElementId(documentSlug, pageNumber);
 
+          // Changing the class name means subsequent runs of the loader won't 
+          // recognize this element as a stub and thus won't enhance it again.
           stub.className = 'DC-embed';
-          stub.setAttribute('id', element_id);
+          stub.setAttribute('id', elementId);
 
           var embedOptions       = extractOptionsFromStub(stub);
-          embedOptions.page      = page_number;
-          embedOptions.container = '#' + element_id;
+          embedOptions.page      = pageNumber;
+          embedOptions.container = '#' + elementId;
 
           DocumentCloud.embed.loadPage(
-            '//www.documentcloud.org/documents/' + document_id + '.json',
+            '//www.documentcloud.org/documents/' + documentSlug + '.json',
             embedOptions
           );
         } else {
@@ -56,22 +86,12 @@
       });
     };
 
-    if (!document.querySelector('link[href$="page_embed.css"]')) {
-      var stylesheet   = document.createElement('link');
-      stylesheet.rel   = 'stylesheet';
-      stylesheet.type  = 'text/css';
-      stylesheet.media = 'screen';
-      stylesheet.href  = 'dist/page_embed.css';
-      document.querySelector('head').appendChild(stylesheet);
-    }
-
+    // Definitions are complete. Do things!
+    insertStylesheet();
     if (window.DocumentCloud) {
       enhanceStubs();
-    } else if (!document.querySelector('script[src$="page_embed.js"]')) {
-      var page_embed_js = document.createElement('script');
-      page_embed_js.src = "dist/page_embed.js";
-      Penny.on(page_embed_js, 'load', enhanceStubs);
-      document.querySelector('body').appendChild(page_embed_js);
+    } else {
+      insertJavaScript();
     }
 
   });
