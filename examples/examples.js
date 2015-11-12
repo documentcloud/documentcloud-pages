d911eb3ca2cef022a39043ed10f687d9614ec481
Justin Reese <justin@justinreese.com>
2015-11-12 13:14:46 -0600

    Move iframed version to its own page
    We won't be baking in our own responsive iframe solution for a while longer (see #18), and the test page was inadvertently telegraphing too much intent. Move to a subpage.

---
+++ examples/examples.js
@@ -0,0 +1,42 @@
+(function(){
+  if (window.DocumentCloud) {
+    var $ = DocumentCloud.$;
+    var _ = DocumentCloud._;
+
+    // Cache width indicators and their parents so we don't have to do
+    // expensive DOM lookups during `resize`;
+    var $widths = $('.width');
+    $widths.each(function(){
+      $(this).data('target', $(this).closest('.width-target'));
+    });
+    var updateColWidths = function(){
+      $widths.each(function(){
+        $(this).text( $(this).data('target').css('width') );
+      });
+    };
+    updateColWidths();
+    $(window).on('resize', function() {
+      updateColWidths();
+    })
+
+    // Live option changing
+    $('.option').on('change', function(){
+      var optionName = $(this).data('option');
+      var optionVal  = $(this).prop('checked');
+      _.each(DocumentCloud.embed.views.pages, function(containers, documentId) {
+        _.each(containers, function(view, elementId) {
+          if (view.openNote) {
+            view.openNote.close();
+          }
+          view.options[optionName] = optionVal;
+          view.$el.html(view.render());
+        });
+      });
+    });
+
+    // Ajax-inserted embed code test
+    $.get('embed_code.html', function(data) {
+      $('.embed_via_ajax').html(data);
+    });
+  }
+})();
