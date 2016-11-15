03ec40ef3874d6af7ccdeb26efa42c9484645945
Justin Reese <justin@justinreese.com>
2016-11-15 15:18:58 -0600

    Add semicolons at EOL
    Fixing royal screwup in 07666535af1edd8142b471ad35cd1a189a47706e

--- src/js/util/setup.js
+++ src/js/util/setup.js
@@ -1,8 +1,8 @@
 (function(){
   var DocumentCloud = window.DocumentCloud = window.DocumentCloud || {};
 
-  DocumentCloud.$        = DocumentCloud.$        || window.jQuery.noConflict()
-  DocumentCloud._        = DocumentCloud._        || window._.noConflict(),
-  DocumentCloud.Backbone = DocumentCloud.Backbone || window.Backbone.noConflict()
+  DocumentCloud.$        = DocumentCloud.$        || window.jQuery.noConflict();
+  DocumentCloud._        = DocumentCloud._        || window._.noConflict();
+  DocumentCloud.Backbone = DocumentCloud.Backbone || window.Backbone.noConflict();
   DocumentCloud.embed    = DocumentCloud.embed    || {data: {},definition: {},views: {}};
 })();
